fb45707ec0ac9dc2ad814665d39ff2bac6d1a78d
kailasmsineesh-ai <kailasmsineesh@gmail.com>
2026-02-26 10:08:42 +0530

    add base.html and include templates inheritance,created the allproducts page,added product tables

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    CATEGORY = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    BRAND = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="products")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    offer_rate = models.PositiveIntegerField(default=0)  
+    stock = models.PositiveIntegerField(default=0)
+    description = models.TextField(blank=True, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def allproducts(request):
+    return render(request,'allproducts.html')
